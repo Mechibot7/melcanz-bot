542001468af3bc807b09f2de784a11488638f610
Mechibot7 <84905641+Mechibot7@users.noreply.github.com>
2022-01-17 19:32:48 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+let fs = require('fs')
+let chalk = require('chalk')
+
+global.owner = ['60103729924', '60179842767']
+
+global.APIs = {
+  amel:  'https://melcanz.com',
+}
+global.APIKeys = {
+  'https://melcanz.com': 'JalxrJq4'
+}
+
+global.packname = 'ig @ @'
+global.author = '© mikey & takamechi'
+global.wm = '© mikey & takamechi'
+global.eror = '*Error*'
+global.wait = '*tunggu..*'
+global.benar = '✅'
+global.salah = '❌'
+global.dikit = 'dikit lagi'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("memperbaharui 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
